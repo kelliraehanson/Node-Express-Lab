71771dc5535eab44a1907de793fee5d4a7309329
Kelli Rae Hanson <kelliraehanson@gmail.com>
2019-02-14 15:46:38 -0600

    commit

--- data/db.js
+++ data/db.js
@@ -1,9 +1,8 @@
 const knex = require('knex');
+const knexConfig = require('../knexfile.js');
+// const db = process.env.DB_ENV || 'development';
 
-const db = process.env.DB_ENV || 'development';
-
-const knexConfig = require('../knexfile.js')[db];
-// const db = knex(knexConfig.development);
+const db = knex(knexConfig.development);
 
 module.exports = {
   find,
